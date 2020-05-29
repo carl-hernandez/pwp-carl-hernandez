e6b92604846ae9c4d02d56bfcf1c5fdf55b1089c
Carl Hernandez <carl.hernandez89@gmail.com>
2020-05-29 10:51:57 -0600

    made lots of changes including contact form and changes to the front page

---
+++ public_html/js/srcipt.js
@@ -0,0 +1,121 @@
+$(document).ready (function() {
+	$("#contact").validate({
+		debug: true,
+		errorClass: "alert alert-danger",
+		errorLabelContainer: "#output-area",
+		errorElement: "div",
+
+		// each rule starts with the form input Element's Name attribute'
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			message: {
+				required: true,
+				maxlength: 2000
+			}
+		},
+		messages: {
+			name:{
+				required: "Please enter your name"
+			},
+			email: {
+				required: "Please enter your email",
+				email: "Invalid Email"
+			},
+			message: {
+				required: "Please enter your message",
+				maxlength: "Please limit message to 2000 characters"
+			}
+		},
+
+		submitHandler: function (form) {
+			$("#contact").ajaxSubmit ({
+				type: "POST",
+				url: $("#contact").attr("action"),
+				success: function (ajaxOutput) {
+					$("#output-area").css("display", "")
+					$("#output-area").html(ajaxOutput)
+
+					if ($(".alert-success").length >= 1) {
+						$("#contact")[0].reset();
+					}
+				}
+			})
+		}
+	})
+})
+
+// $(document).ready(function () {
+// 	/**
+// 	 * jQuery Validate Function
+// 	 *
+// 	 * This function provides front-end validation for your form.
+// 	 * If all tests set up here pass, the form data is AJAX submitted
+// 	 * to the mailer.php file.
+// 	 *
+// 	 * Update this file as needed for your form.
+// 	 * All ids and name values must match up to your form here.
+// 	 *
+// 	 * @author Rochelle Lewis <rlewis37@cnm.edu>
+// 	 **/
+//
+// 	$("#contact").validate({
+// 		debug: true,
+// 		errorClass: "alert alert-danger",
+// 		errorLabelContainer: "#output-area",
+// 		errorElement: "div",
+// 		// rules here define what is good or bad input
+// 		//each rule starts with the form input element's NAME attribute
+// 		rules: {
+// 			name: {
+// 				required:true
+// 			},
+// 			email: {
+// 				email: true,
+// 				required: true
+// 			},
+// 			message: {
+// 				required: true,
+// 				maxlength:2000
+// 			}
+// 		},
+// 		//error message to display to the end user when rules above dont pass
+//
+// 		messages: {
+// 			name: {
+// 				required: "Name is required field"
+// 			},
+// 			email: {
+// 				required: "Email is a required field",
+// 				email: "Invalid Email"
+// 			},
+// 			message: {
+// 				required: "message is required",
+// 				maxlength: "Message is too long"
+// 			}
+// 		},
+//
+// 		submitHandler: function (form) {
+// 			$("#contact").ajaxSubmit({
+// 				type: "POST",
+// 				url: $("#contact").attr("action"),
+// 				success: function (ajaxOutput) {
+// 					$("#output-area").css("display", "")
+// 					$("#output-area").html(ajaxOutput)
+//
+// 					// reset the form if it was successful
+// 					if($(".alert-success").length >= 1) {
+// 						$("#contact")[0].reset();
+// 					}
+//
+//
+// 				}
+// 			})
+//
+// 		}
+// 	})
---
+++ src/index.js
@@ -0,0 +1,158 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const mailgun = require('mailgun-js')
+const bodyParser = require('body-parser')
+const {check, validationResult} = require('express-validator')
+
+//initializing express application
+const app = express()
+
+//project wide middleware declarations for Express
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({entended:false}))
+app.use(bodyParser.json())
+
+const indexRoute = express.Router()
+
+const indexRoute = express.Router()
+
+const indexRouteMiddleware = (request, response) => {
+	response.json('is this thing on?')
+}
+
+const requestValidation =[
+	check('email', "A valid email is required").isEmail().normalizeEmail(),
+	check('name, "A name is required to send an email').not().isEmpty().trim().escape(),
+	check('subject').optional().trim().escape(),
+	check('message', 'A message is required to send email').not().isEmpty().trim().escape().isLength({max:2000})
+
+]
+
+indexRoute.route('/apis')
+	.get(indexRouteMiddleware)
+	.post(requestValidation, (request, resposne) =>{
+		const errors = validationResult(request)
+
+		if(!errors.isEmpty()) {
+			const currentError = errors.array()[0]
+			console.log(currentError.msg)
+			return response.send(Buffer.from(`<div class ='alert alter-danger' role='alert><strong>Oh snap!</strong> ${currentError.msg}</div>`))
+		}
+		const {name, email, subjct, message} = request.body
+
+		const domain = process.env.MAILGUN_DOMAIN
+
+		const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain})
+
+		const mailgunData = {
+			to: process.env.MAIL_RECIPIENT,
+			from: `Mailgun Sandbox <postmaster@${domain}>`,
+			subject: `${name} - ${email}: ${subject}`,
+			text: message
+		}
+
+		mg.messages().send(mailgunData, (error) =>{
+			if (error) {
+				response.send(Buffer.from(`
+<div class='alert alter-danger' role='alert'><strong>Oh snap!</strong> Unable to send email error with email sender</div>`
+				))
+			}
+		})
+		// Line below commented out before hosting
+		response.append('Access-Control-Allow-Origin', ['*'])
+		response.append('Content-Type', 'text/html')
+
+		return response.send(Buffer.from("<div class='alert alert-success' role='alert'>Email successfully sent.</div>))
+	})
+app.use(indexRoute)
+
+app.listen(4200, () => {console.log('The server has started')})
+
+// 	.get((request, response) => {
+// 	return response.(Buffer.from(`<div class='alert alter-danger' role='alert'><strong>Oh snap!</strong> ${currentError.msg}</div>`))
+// })
+// 	.post(requestValidation, (request, response) => {
+// 		response.append('Content-type', 'text/html')
+//
+// 		//the line below must be commented out before pwp has been hosted using docker
+// 		response.apped('Access-Control-Allow-Origin', ['*'])
+// 		const domain = process.env.MAILGUN_DOMAIN
+// 		const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain})
+//
+// 		const {email, subject, name, message} = request.body
+//
+// 		const mailgunData = {
+// 			to: process.env.MAIL_RECIPIENT,
+// 			from: `Mailgun Sandbox <postmaster@${domain}>`,
+// 			subject: `${name} - ${email}: ${subject}`,
+// 			text: message
+// 		}
+//
+// 		mg.messages().send(mailgunData, (error) =>{
+// 		if (error) {
+// 			response.send(Buffer.from(`
+// <div class='alert alter-danger' role='alert'><strong>Oh snap!</strong> Unable to send email error with email sender</div>`
+// 			))
+// 		}
+// 	})
+//
+//
+// 		const errors = validationResult(request)
+//
+// 		if(!errors.isEmpty()) {
+// 		const currentError = errors.()[0]
+// 		return response.json(`bad request: ${currentError.msg}`)
+// 		}
+//
+//
+// 		return.response.send(Buffer.from("<div class='alert alert-success' role='alert'>Email successfully sent.</div>))
+// 	})
+//
+// app.use(indexRoute)
+//
+// app.listen(4200, () => {console.log('The server has started')})
+
+		//lines below are used above
+		// check('email', "A valid email is required").isEmail().normalizeEmail(),
+		// check('name, "A name is required to send an email').not().isEmpty().trim().escape(),
+		// check('subject').optional().trim().escape(),
+		// check('message', 'A message is required to send email').not().isEmpty().trim().escape().isLength({max:2000})
+
+// 	],
+// 		(request, response) => {
+//
+// 			const domain = process.env.MAILGUN_DOMAIN
+// 			const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
+//
+// 		const {email, subject, name, message} = request.body
+//
+// 			const data = {
+// 				to: process.env.MAIL_RECIPIENT,
+// 				from: `Mailgun Sandbox <postmaster@${domain}>`,
+// 				subject: `${name} - ${email}: ${subject}`,
+// 				text: message
+// 			}
+//
+// 			mg.messages().send(mailgunData, (error) => {
+// 				return response.json('error sending email through email handler please try again')
+// 			})
+//
+// 		const errors= validationResult(request)
+//
+// 		if(!errors.isEmpty()) {
+// 			const currentError = errors.array()[0]
+// 			return response.json(`bad request: ${currentError.msg}`)
+// 		}
+// 		//this line below must be commented out before pwp hosting using docker
+// 		response.append('Access-Control-Allow-Origin', ['*'])
+// 		console.log(request.body)
+// 		return response.send(Buffer.from('<div class='alert alert-success' role='alert'>Email successfully sent.</div>'))
+// 	})
+//
+// app.use(indexRoute)
+//
+// app.listen(4200, () => {console.log('The server has started')})
+//
+//
